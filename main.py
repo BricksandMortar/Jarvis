ec6a04e14f25c5d4e9dfe10a1a160d96304d3c20
Arran France <arranf@gmail.com>
2016-04-26 01:28:47 +0100

    Add Travis configuration to setup script

--- main.py
+++ main.py
@@ -2,19 +2,10 @@ import argparse
 import logging
 import sys
 import shutil
-import json
+import requests
 
-
-from os import path, mkdir, close, remove
+from os import path, mkdir, close, remove, scandir
 from tempfile import mkstemp
-from urllib.request import urlopen, Request
-
-
-
-try:
-    from os import scandir, walk
-except ImportError:
-    from scandir import scandir, walk
 
 
 # Recursively loop over the directory structure until it finds a file.
@@ -44,6 +35,13 @@ def generate_directories(dirpath):
     exists_directory(dirpath)
 
 
+def get_token():
+    with open('./token.txt') as travis_token_file:
+        for line in travis_token_file:
+            if line.__contains__('token'):
+                return line.rpartition(' ')[1]
+
+
 # Recursive algorithm that builds directories if they don't exist
 def exists_directory(dirpath):
     if dirpath.endswith('\\') or dirpath.endswith('/'):
@@ -57,8 +55,6 @@ def exists_directory(dirpath):
         new_path = dirpath[:split]
         exists_directory(new_path)
         mkdir(path.abspath(dirpath))
-
-# Takes a directory and attempts to get the .git directory
 
 # Takes a repository path and gets a Github repo name from it
 def get_github_repo_name(git_dir):
@@ -90,6 +86,7 @@ def get_github_org_name(git_dir):
     else:
         logging.warning('No .git directory found')
 
+
 # http://stackoverflow.com/questions/39086/search-and-replace-a-line-in-a-file-in-python
 def replace(file_path, pattern, subst):
     # Create temp file
@@ -104,14 +101,27 @@ def replace(file_path, pattern, subst):
     # Move new file
     shutil.move(abs_path, file_path)
 
+def trigger_build(out_dir, repo_id):
+    if path.exists(out_dir + '.travis.yml'):
+        travis_headers['Travis-API-Version'] = '3'
+        travis_build_url = 'https://api.travis-ci.org/repo/' + repo_id + '/requests'
+        travis_build_data = {
+            "request": {
+                "branch": "pages-ci"
+            }}
+        travis_build_request = requests.post(travis_build_url, json=travis_build_data, headers=travis_headers)
+        if travis_build_request.status_code != 200 or travis_build_request.status_code != 202:
+            travis_build_request.raise_for_status()
+
 
 parser = argparse.ArgumentParser(description="Setups a jekyll template in a gh-pages repository")
 parser.add_argument("--input", required=True, help="The directory where the jekyll template is stored", dest='in_path')
 parser.add_argument("--output", nargs='?', default='./', const='./', help="The directory of the gh-pages repository",
                     dest='out_path')
 parser.add_argument("--user-name, --org-name", nargs='?', default=False, const=False, help="The name of the user or organisation the repository belongs to. If not provided it is attempted to be scraped from the .git directory", dest='org_name')
+parser.add_argument("--token", nargs='?', default=None, const=None, help="The token used to authenticate Travis API calls", dest='token')
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 args = parser.parse_args()
 
@@ -120,18 +130,19 @@ args_copy = args.__dict__
 # * All end in '/', important for joining together the paths
 # * Exist
 for key, value in args_copy.items():
-    if ('path') in key:
+    if 'path' in key:
         value = path.abspath(value)
         if not (value.endswith('/') or value.endswith('\\')):
             value += '/'
         value = value.replace('\\', '/')
         args_copy[key] = value
-    if not path.exists(value):
-        raise ValueError('Directory specified {} not found'.format(value))
+        if not path.exists(value):
+            raise ValueError('Directory specified {} not found'.format(value))
 
 in_path = args_copy['in_path']
 out_path = args_copy['out_path']
 org_name = args_copy['org_name']
+token = args_copy['token']
 
 # May not exist
 git_dir = (out_path + '.git')
@@ -185,35 +196,29 @@ else:
 # Query which branches exist for the repo
 # https://api.github.com/repos/:org/:repo/branches
 branches = set()
-url = 'https://api.github.com/repos/' + org_name + '/' + repo_name + '/branches'
-logging.info('Querying ' + url)
-branch_request = Request(url)
-with urlopen(branch_request) as response:
-    if response.status == 200:
-        # Get response as a string rather than a series of bytes
-        str_response = response.read().decode('utf-8')
-        branch_response = json.loads(str_response)
-        for branch in branch_response:
-            branch_name = branch['name']
-            if branch_name != 'gh-pages' and branch_name != 'pages-ci':
-                branches.add(branch['name'])
-        for branch in branches:
-            logging.info('Branch found: ' + branch)
-    else:
-        logging.warning(response.status)
-        sys.exit()
+branches_url = 'https://api.github.com/repos/' + org_name + '/' + repo_name + '/branches'
+logging.info('Querying ' + branches_url)
+branch_request = requests.get(branches_url)
+if branch_request.status_code != 200:
+    logging.warning('Response was: ' + str(branch_request.status_code))
+else:
+    for branch in branch_request.json():
+        branch_name = branch['name']
+        if branch_name != 'gh-pages' and branch_name != 'pages-ci':
+            branches.add(branch['name'])
+    for branch in branches:
+        logging.info('Branch found: ' + branch)
 
 # Find file tree and ignore for each branch in branches
 # https://api.github.com/repos/:org/:repo/git/trees/:branch?recursive=1
-files = set()
-for branch in branches:
-    file_tree_url = 'https://api.github.com/repos/' + org_name + '/' + repo_name + '/git/trees/' + branch + '?recursive=1'
-    logging.info('Querying ' + url)
-    file_tree_request = Request(file_tree_url)
-    with urlopen(file_tree_request) as response:
-        if response.status == 200:
-            str_response = response.read().decode('utf-8')
-            file_tree_response = json.loads(str_response)
+if branches is not None:
+    files = set()
+    for branch in branches:
+        file_tree_url = 'https://api.github.com/repos/' + org_name + '/' + repo_name + '/git/trees/' + branch + '?recursive=1'
+        logging.info('Querying ' + branches_url)
+        file_tree_request = requests.get(file_tree_url)
+        if file_tree_request.status_code == 200:
+            file_tree_response = file_tree_request.json()
             tree = file_tree_response['tree']
             for item in tree:
                 if item['type'] == 'blob':
@@ -221,14 +226,71 @@ for branch in branches:
         else:
             logging.warning('Request failed: ' + file_tree_request.__str__())
 
-# If no files to write to .gitignore, quit
-if len(files) > 0:
-    logging.info(len(files).__str__() + ' files found to ignore')
+    # If no files to write to .gitignore, quit
+    if len(files) > 0:
+        logging.info(len(files).__str__() + ' files found to ignore')
 
-    # Write file paths to .gitignore file
-    with open(out_path + '.gitignore', 'a') as gitignore:
-        gitignore.write('\n#Ignore copied from other repo branch\n')
-        for file_path in files:
-            if check_file(file_path):
-                gitignore.write(file_path + '\n')
-        gitignore.write('#Endignore copied from other repo branch\n')
+        # Write file paths to .gitignore file
+        with open(out_path + '.gitignore', 'a') as gitignore:
+            gitignore.write('\n#Ignore copied from other repo branch\n')
+            for file_path in files:
+                if check_file(file_path):
+                    gitignore.write(file_path + '\n')
+            gitignore.write('#Endignore copied from other repo branch\n')
+
+branches_url = 'https://api.github.com/repos/' + org_name + '/' + repo_name + '/branches'
+logging.info('Querying ' + branches_url)
+branch_request = requests.get(branches_url)
+if branch_request.status_code == 200:
+    branch_response = branch_request.json()
+    for branch in branch_response:
+        branch_name = branch['name']
+        if branch_name != 'gh-pages' and branch_name != 'pages-ci':
+            branches.add(branch['name'])
+    for branch in branches:
+        logging.info('Branch found: ' + branch)
+else:
+    logging.warning('Response: ' + str(branch_request.status_code))
+
+# Get travis auth token
+if token is None:
+    token = get_token()
+
+if token is not None:
+    logging.info('Token is :' + token)
+else:
+    logging.warning('No token could be found')
+
+travis_headers = {'Accept': 'application/json', 'Authorization': 'token ' + token}
+
+# Get repo id
+travis_repo_id_url = 'https://api.travis-ci.org/repos/BricksandMortar/' + repo_name
+logging.info('Querying ' + travis_repo_id_url)
+travis_repo_id_request = requests.get(travis_repo_id_url, headers=travis_headers)
+if travis_repo_id_request.status_code == 200:
+    travis_repo_id_response = travis_repo_id_request.json()
+    repo_id = str(travis_repo_id_response['id'])
+    logging.info('Repo Id: ' + repo_id)
+else:
+    travis_repo_id_request.raise_for_status()
+    sys.exit()
+
+# Add repo to Travis
+travis_repo_add_url = 'https://api.travis-ci.org/hooks/'+repo_id
+travis_add_data = {'hook[active]': 'true'}
+logging.info('Adding Repo to Travis')
+travis_repo_add_request = requests.put(travis_repo_add_url, headers=travis_headers, data=travis_add_data)
+if travis_repo_add_request.status_code != 200:
+    travis_repo_add_request.raise_for_status()
+    sys.exit()
+
+# Ensure setting to only branches with .travis.yml present
+logging.info('Configuring only to build this branch')
+travis_settings_url = 'https://api.travis-ci.org/repos/'+repo_id+'/settings'
+travis_settings_data = {"settings": {
+  "builds_only_with_travis_yml": "true"}
+}
+travis_settings_request = requests.patch(travis_settings_url, json=travis_settings_data, headers=travis_headers)
+if travis_settings_request.status_code != 200:
+    travis_settings_request.raise_for_status()
+
